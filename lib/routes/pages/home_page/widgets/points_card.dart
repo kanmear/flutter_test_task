529bf312cacd60a3f63f7f8bb60413c471923fce
mind_blade <kanmear+github@gmail.com>
2024-09-21 23:20:55 +0300

    feat: points card -> bottles picture

--- lib/routes/pages/home_page/widgets/points_card.dart
+++ lib/routes/pages/home_page/widgets/points_card.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:live_beer/text_data.dart';
 
 class PointsCard extends StatelessWidget {
@@ -15,33 +16,43 @@ class PointsCard extends StatelessWidget {
     final halfOfContainerWidth =
         ((MediaQuery.of(context).size.width - paddings * 2) / 2).floor();
 
-    return Container(
-      padding: const EdgeInsets.all(paddings),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: theme.colorScheme.onPrimary),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            _getRandomValue(),
-            style: theme.textTheme.displayMedium!.apply(color: textColor),
-          ),
-          const SizedBox(height: 4),
-          Text(
-            TextData.accumulatedPoint,
-            style: theme.textTheme.bodyMedium!.apply(color: textColor),
-          ),
-          const SizedBox(height: 8),
-          SizedBox(
-            width: halfOfContainerWidth.toDouble(),
-            child: Text(
-              TextData.collectPoints,
-              style: theme.textTheme.labelSmall!
-                  .apply(color: textColor.withOpacity(0.7)),
+    return ClipRect(
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: theme.colorScheme.onPrimary),
+        child: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(paddings),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    _getRandomValue(),
+                    style:
+                        theme.textTheme.displayMedium!.apply(color: textColor),
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    TextData.accumulatedPoint,
+                    style: theme.textTheme.bodyMedium!.apply(color: textColor),
+                  ),
+                  const SizedBox(height: 8),
+                  SizedBox(
+                    width: halfOfContainerWidth.toDouble(),
+                    child: Text(
+                      TextData.collectPoints,
+                      style: theme.textTheme.labelSmall!
+                          .apply(color: textColor.withOpacity(0.7)),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            const BottlePicture()
+          ],
+        ),
       ),
     );
   }
@@ -49,5 +60,24 @@ class PointsCard extends StatelessWidget {
   _getRandomValue() {
     final random = Random();
     return (random.nextInt(3017) + 1).toString();
+  }
+}
+
+class BottlePicture extends StatelessWidget {
+  const BottlePicture({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Positioned(
+      right: 1,
+      top: 1,
+      child: SvgPicture.asset(
+        'assets/svg/bottles.svg',
+        colorFilter:
+            ColorFilter.mode(theme.colorScheme.primary, BlendMode.srcIn),
+      ),
+    );
   }
 }
